208d9351c09681033226b20be5a56a094d7bdd37
Paul Niemczyk <mrpndev@gmail.com>
2026-03-17 19:00:25 -0400

    progress

--- app.js
+++ app.js
@@ -1,45 +1,76 @@
 let apiEndpoint = `https://pokeapi.co/api/v2/pokemon/`;
+const cardContainer = document.querySelector(".card-container")
+
 
 let input = document.querySelector("#input");
 document.querySelector(".btn-primary").addEventListener("click", async evt => {
 	evt.preventDefault();
 	let url = `${apiEndpoint}${input.value}`;
-	console.log(url);
 	let result = await getData(url);
-	render(result);
+
+	if (result) {
+		render(result);
+	}
 });
 
 async function getData(url) {
 	try {
-		// let res = await fetch(url)
-		// return await res.json()
-		return await (await fetch(url)).json();
+		let res = await fetch(url)
+
+		// Guard clauses programming style
+		if (!res.ok) {
+			throw new Error(`Server Error`, { cause: res.status })
+		}
+		return await res.json()
 	} catch (err) {
-		console.log(err);
+		console.log(err)
+		renderErrorHandler(err)
+	}
+}
+
+function renderErrorHandler(err) {
+	cardContainer.classList.add("hidden")
+	let p = document.createElement("p")
+
+	if (err.cause === 404) {
+		p.textContent = `Pokemon not found. Try another Pokemon.`
+		cardContainer.insertAdjacentElement("afterend", p)
+	}
+	
+	if (err.cause === 400) {
+		p.textContent = `URL Issues`
+		cardContainer.insertAdjacentElement("afterend", p)
+	}
+
+	if (err.cause === 500) {
+		p.textContent = `Server Error. Try again Later.`
+		cardContainer.insertAdjacentElement("afterend", p)
 	}
 }
 
 function render(data) {
-	console.log(data);
 	const img = document.querySelector(".card-img-top");
 	const pokemonName = document.querySelector(".pokemon-name");
 	const weight = document.querySelector(".weight");
 	const baseExp = document.querySelector(".base-experience");
 	const abilities = document.querySelector(".abilities");
-	console.dir(abilities.textContent)
+
 	// Simple solution to childElementCount
 	abilities.textContent = ""
 	img.src = data.sprites.front_default;
 	pokemonName.textContent = capitalizeFirstLetter(data.name)
 	weight.textContent = `Weight: ${data.weight}`
 	baseExp.textContent = `Base Experience: ${data.base_experience}`
+
+	if (pokemonName.textContent) {
+		cardContainer.classList.remove("hidden")
+	}
 	
 	// if (abilities.childElementCount) {
 	// 	abilities.replaceChildren()
 	// }
 
 	data.abilities.forEach(a => {
-		console.log(a.ability.name)
 		let aBtn = document.createElement("a")
 		aBtn.classList.add("btn", "btn-primary")
 		aBtn.textContent = capitalizeFirstLetter(a.ability.name)
